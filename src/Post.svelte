00d2b6265d1802a765a0508884addb5f78fa4e91
Chris Weiskopf <chris@chriscancode.io>
2023-02-28 15:49:27 -0500

    updating dotenv

--- src/Post.svelte
+++ src/Post.svelte
@@ -1,8 +1,6 @@
 <script>
   import { fade } from "svelte/transition";
   import Editor from '@tinymce/tinymce-svelte';
-  import * as dotenv from 'dotenv'
-  dotenv.config()
 
   let postError = "";
   let postTitle = "";
